96114dc242c496ce4034458dce6e28f4eb55c0d6
noman5945 <abdullahnoman.5945@gmail.com>
2024-01-11 01:03:13 +0600

    authentication using jwt token work done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,23 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("doctors portal server is running");
 });
+
+function veriftyJWT(req, res, next) {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      console.log(err.message);
+      return res.status(403).send({ messege: "forbidden access" });
+    }
+    req.decoded = decoded;
+    console.log(decoded);
+    next();
+  });
+}
 
 const DBUser = process.env.DB_USER;
 const DBPass = process.env.DB_PASS;
@@ -39,6 +57,7 @@ async function run() {
       .db("Doctors-Portal")
       .collection("bookings");
 
+    const usersCollection = client.db("Doctors-Portal").collection("users");
     /*API s */
     /*Options from database */
 
@@ -117,8 +136,12 @@ async function run() {
     });
 
     /*Bookings */
-    app.get("/clientBookings", async (req, res) => {
+    app.get("/clientBookings", veriftyJWT, async (req, res) => {
       const clientEmail = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if (clientEmail !== decodedEmail) {
+        return res.status(403).send("forbidden access");
+      }
       const query = { Email: clientEmail };
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
@@ -141,6 +164,28 @@ async function run() {
       }
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+
+    // adding new user to mongodb
+    app.post("/addUser", async (req, res) => {
+      const user = req.body;
+      const query = {};
+      const addingUser = await usersCollection.insertOne(user);
+      res.send(addingUser);
+    });
+
+    // json web token works
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { Email: email };
+      const getExistingUser = await usersCollection.find(query);
+      if (getExistingUser) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accesToken: token });
+      }
+      return res.status(403).send({});
     });
   } finally {
   }
