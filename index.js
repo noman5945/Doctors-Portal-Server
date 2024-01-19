9457fdce39bf5fc49f564dbf8f1f3d0b0ffac517
noman5945 <abdullahnoman.5945@gmail.com>
2024-01-19 22:00:40 +0600

    confirming payment through credit card

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRETE_KEY);
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -280,6 +281,21 @@ async function run() {
       res.send(deleted_doc);
 
       /**********************************Doctors handling API End******************************************************* */
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.Price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     /****************Extras*************** */
